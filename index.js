567751f5e9d9bd6cb97e99285a0bc05e43a808cf
Jan-Philipp Marks <jpm3492@gmail.com>
2018-03-19 17:33:49 +0100

    new cli

--- index.js
+++ index.js
@@ -4,21 +4,19 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-
+const path = require('path');
 
 const keys = require('./config/keys');
 
 const PORT = process.env.PORT || 5000;
 const port = 8000;
+const INDEX = path.resolve(__dirname, 'client', 'build', 'index.html');
 
 require('./models/User');
 require('./models/Highlights');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
-
-const app = express();
-const io = require('socket.io')(app);
 
 app.use(bodyParser.json({limit: '1mb' }));
 app.use(
@@ -30,17 +28,22 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+const app = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const io = require('socket.io')(app);
+
 require('./routes/authRoutes')(app);
 require('./routes/dataRoutes')(app, io);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
-
-io.listen(port);
-app.listen(PORT);
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+//   const path = require('path');
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+//
+//
+// io.listen(port);
+// app.listen(PORT);
