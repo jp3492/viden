bf95e0b1551c49094cee915b264a3c32a9818203
Jan-Philipp Marks <jpm3492@gmail.com>
2018-03-20 13:57:28 +0100

    trying for sockets

--- client/src/reducer/AuthReducer.js
+++ client/src/reducer/AuthReducer.js
@@ -3,6 +3,9 @@ import { FETCH_USER, LOGOUT } from '../actions/types';
 export default function ( state = false, action ){
   switch (action.type) {
     case FETCH_USER:
+      if (action.payload === "not logged in") {
+        return false;
+      }
       return { _id: action.payload._id, firstName: action.payload.firstName, lastName: action.payload.lastName, email: action.payload.email, approved: action.payload.approved } || false;
     case LOGOUT: return false;
     default:
