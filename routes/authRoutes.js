4247972a006a06e82c917a20883cc22f82706122
Jan-Philipp Marks <jpm3492@gmail.com>
2018-03-20 14:06:18 +0100

    trying for sockets

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -23,38 +23,34 @@ module.exports = app => {
   });
 
   app.get('/api/current_user', async (req, res) => {
-    if (req.user !== undefined) {
-      const { _id } = req.user;
-      let projects = await Highlights.find({ _uid: _id });
-      let user = req.user._doc;
-      let foreignProjects = user.access.filter( a => { return (a.type === "project" && a.status === "confirmed") });
-      foreignProjects = await Promise.all(foreignProjects.map( async p => {
-        const project = await Highlights.findById(p.target);
-        return project;
-      }));
-      foreignProjects = foreignProjects.filter( p => { return p._uid.toString() !== id.toString() });
-      projects = projects.concat(foreignProjects);
-      user = { ...req.user._doc, projects };
-      const friends = await Promise.all(user.friends.map( async f => {
-        const reqUser = await User.findById(f._id);
-        return { ...f._doc, firstName: reqUser.firstName, lastName: reqUser.lastName };
-      }));
-      const access = await Promise.all(user.access.map( async a => {
-        const reqUser = await User.findById(a.user);
-        let reqTarget;
-        if (a.type === "project") {
-          reqTarget = await Highlights.findById(a.target);
-          reqTarget = reqTarget.title;
-        } else {
-          reqTarget = reqUser.folders.filter( f => { return f._id === a.target });
-          reqTarget = reqTarget.name;
-        }
-        return { ...a._doc, firstName: reqUser.firstName, lastName: reqUser.lastName, name: reqTarget };
-      }));
-      user = { ...user, friends, access };
-      res.send(user);
-    } else {
-      res.send("not logged in");
-    }    
+    const { _id } = req.user;
+    let projects = await Highlights.find({ _uid: _id });
+    let user = req.user._doc;
+    let foreignProjects = user.access.filter( a => { return (a.type === "project" && a.status === "confirmed") });
+    foreignProjects = await Promise.all(foreignProjects.map( async p => {
+      const project = await Highlights.findById(p.target);
+      return project;
+    }));
+    foreignProjects = foreignProjects.filter( p => { return p._uid.toString() !== id.toString() });
+    projects = projects.concat(foreignProjects);
+    user = { ...req.user._doc, projects };
+    const friends = await Promise.all(user.friends.map( async f => {
+      const reqUser = await User.findById(f._id);
+      return { ...f._doc, firstName: reqUser.firstName, lastName: reqUser.lastName };
+    }));
+    const access = await Promise.all(user.access.map( async a => {
+      const reqUser = await User.findById(a.user);
+      let reqTarget;
+      if (a.type === "project") {
+        reqTarget = await Highlights.findById(a.target);
+        reqTarget = reqTarget.title;
+      } else {
+        reqTarget = reqUser.folders.filter( f => { return f._id === a.target });
+        reqTarget = reqTarget.name;
+      }
+      return { ...a._doc, firstName: reqUser.firstName, lastName: reqUser.lastName, name: reqTarget };
+    }));
+    user = { ...user, friends, access };
+    res.send(user);
   });
 };
