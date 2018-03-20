5f67747fc8462f5828c3697393805728b319f71f
Jan-Philipp Marks <jpm3492@gmail.com>
2018-03-20 11:34:41 +0100

    trying for sockets

--- index.js
+++ index.js
@@ -40,9 +40,6 @@ const io = require('socket.io').listen(server);
 
 server.listen(PORT);
 
-io.set("transports", ["xhr-polling"]);
-io.set("polling duration", 10); 
-
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
