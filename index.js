de97a18b00a45052d584ea37774a73a60c3174a7
Jan-Philipp Marks <jpm3492@gmail.com>
2018-03-20 15:47:21 +0100

    trying for sockets

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-app.listen(PORT);
+server.listen(PORT);
 
 io.on('connection', socket => {
   console.log("client connected");
