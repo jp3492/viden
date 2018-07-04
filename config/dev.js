d3bd1f9fe5a5552ab68c6f9d09ad6c896f04e224
jp3492 <jpm3492@gmail.com>
2018-07-04 08:25:52 +0200

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 // add accordingly
 module.exports = {
-  googleClientID: '69493395956-3pkhcg0shfktabv1btf0r5cu571v4f61.apps.googleusercontent.com',
-  googleClientSecret: 'iXMpUrBTs0HqEeGSk1vfIAJk',
-  mongoURI: 'mongodb://admin:testing@ds115198.mlab.com:15198/yth-dev',
-  cookieKey: 'skdniend7pw8nwipunpiuwaU'
+  googleClientID: '',
+  googleClientSecret: '',
+  mongoURI: '',
+  cookieKey: ''
 };
